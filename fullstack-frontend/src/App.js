6cf992fdf1a03dc64b4b5de7ff4ed32ed27ae3da
Avry Jackson <158083847+avryjacksoncom@users.noreply.github.com>
2025-03-19 15:17:44 -0700

    cleaned up version to main

--- fullstack-frontend/src/App.js
+++ fullstack-frontend/src/App.js
@@ -2,23 +2,23 @@
 import './App.css';
 import TicTacToe from './pages/TicTacToe';
 import NewGame1 from './pages/NewGame1';
+import NewGame2 from './pages/NewGame1';
 import{BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Navbar />
         <Routes>
           
           <Route exact path="/" element={<TicTacToe/>} />
           <Route exact path="/newgame1" element={<NewGame1/>} />
+          <Route exact path="/newgame2" element={<NewGame2/>} />
           {/* <Route exact path="/projects" element={<ProjectPage />} />
           <Route exact path="/notes" element={<NotesPage />} /> */}
 
          
         </Routes>
-          <Footer />
       </Router>
      
      
